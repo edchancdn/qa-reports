88fbaf91beb8659eb75b5817037fdc1bec2b92cc
edchancdn <edchan.cdn@gmail.com>
2022-05-31 20:48:58 -0400

    added headless flag

--- src/test/java/org/reports/ZoomTest.java
+++ src/test/java/org/reports/ZoomTest.java
@@ -42,7 +42,7 @@ public class ZoomTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
 
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
